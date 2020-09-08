d57ce682b5131455438279d39b777c3c51b96f5a
Alexander Liu <liujalexander84@gmail.com>
2020-09-08 13:44:36 -0700

    get the timer function thing in place

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello");
+import moment from "moment";
+
+// add ten so that it doesnt trigger twice every hour occasionally
+const getnexttime = () => moment().endOf("hour").toDate().valueOf() - moment().valueOf() + 10;
+
+let i = 0;
+
+function lol() {
+   console.log(++i);
+   setTimeout(lol, getnexttime());
+}
+
+lol();
+// get which hour of the day it is
+// get end of hour
+// set it to that wallpaper
+// repeat yoy
+
+// PROBLEM: could fire twice occasionally
